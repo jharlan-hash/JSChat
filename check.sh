95a03e769b3248d6bc9096c9d37aa353f476380d
Jack Sovern <25jaso@decorah.school>
2024-11-28 21:36:02 -0600

    AES.java

--- check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-touch ./source
-cat ./src/* > ./source
-nvim -c "CopilotChat /COPILOT_REVIEW Review the four files concatenated together below (the files will be separated by comments): $(cat ./source)"
-rm ./source
